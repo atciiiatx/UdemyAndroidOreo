adb8e8b751e267371168764db25ba6f0d81ec74a
A. T. Campbell, III <atcampbelliii@gmail.com>
2018-07-06 17:27:08 -0500

    Add button

--- ExampleApp/app/src/main/java/com/hookbang/android/exampleapp/MainActivity.java
+++ ExampleApp/app/src/main/java/com/hookbang/android/exampleapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.hookbang.android.exampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view)
+    {
+        Log.i("Info", "Button pressed!");
     }
 }
